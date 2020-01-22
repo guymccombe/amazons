a8f18245188abb57b62a6a42f00c4270eb03e0a8
unknown <guymccombe@gmail.com>
2020-01-22 22:24:57 +0000

    Experiments with visdom

--- src/main/python/agent.py
+++ src/main/python/agent.py
@@ -1,10 +1,34 @@
-from .environment import Environment
+from environment import Environment
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+import visdom
 
 
 class Agent():
 
     def __init__(self):
         self.env = Environment()
+        vis = visdom.Visdom(port=8097)
+        if not self.env.isGameFinished():
+            print("Getting initial images")
+            img1, img2, img3 = self.env.getState()
+            vis.image(img1, win="Own amazons")
+            vis.image(img2, win="Opp amazons")
+            vis.image(img3, win="Arrows")
+
+            print("Moving!")
+            self.env.move((0, 3), (0, 4), (0, 3))
+
+            self.env.isGameFinished()
+            print("Getting new images")
+            img1, img2, img3 = self.env.getState()
+            vis.image(img1, win="Own amazons 2")
+            vis.image(img2, win="Opp amazons 2")
+            vis.image(img3, win="Arrows 2")
 
 
 if __name__ == "__main__":
