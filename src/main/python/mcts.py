a6cc58ee1b9092f1594b897f2b77dbb8ba03e6c1
unknown <guymccombe@gmail.com>
2020-02-12 17:56:09 +0000

    Finished MCTS structure
    Next up, testing and game rollback implementation

--- src/main/python/mcts.py
+++ src/main/python/mcts.py
@@ -21,70 +21,159 @@ class mcts():
 
     def search(self):
         if self.env.isGameFinished():
-            return self.env.getReward()
+            # Returns 3-tuple reward
+            return (self.env.getReward(),) * 3
+
+        values = [0, 0, 0]
 
         state = self.env.getState()
         stateString = self.env.toString(state)
         stateTensor = torch.tensor(state, dtype=torch.float).unsqueeze_(0)
 
-        if stateString not in self.policies:  # new leaf node
-            self.policies[stateString], value = self.nets[0](stateTensor)
+        if stateString not in self.policies:
+            self.policies[stateString], values[0] = self.nets[0](stateTensor)
             validSelections = torch.tensor(self.env.getPositionOfAmazons())
 
             self.policies[stateString] *= validSelections  # mask out invalids
 
             validCoordinates = np.nonzero(validSelections)
             self.valids[stateString] = validCoordinates
-            return -value
 
         validCoordinates = self.valids[stateString]
         bestScore = float("-inf")
-        bestAction = None
-        bestActionStr = ""
-        bestActionArr = None
+        bestSelection = None
+        bestSelectionStr = ""
+        bestSelectionArr = None
 
-        for action in validCoordinates:
-            action = tuple(coord.item() for coord in action)
-            actionArr = np.zeros((10, 10), dtype=np.uint8)
-            actionArr[action] = 1
-            actionStateTuple = state + (actionArr,)
-            actionString = self.env.toString(actionStateTuple)
+        for selection in validCoordinates:
+            selection = tuple(coord.item() for coord in selection)
+            selectionArr = np.zeros((10, 10), dtype=np.uint8)
+            selectionArr[selection] = 1
+            selectionTuple = state + (selectionArr,)
+            selectionString = self.env.toString(selectionTuple)
 
-            if actionString not in self.valids:
-                self.valids[actionString] = self.env.getPossibleMovesFrom(
-                    action)
+            if selectionString not in self.valids:
+                self.valids[selectionString] = self.env.getPossibleMovesFrom(
+                    selection)
 
-            if len(np.nonzero(self.valids[actionString])) < 1:
-                self.policies[stateString][action] = 0
+            if len(np.nonzero(self.valids[selectionString])) < 1:
+                self.policies[stateString][selection] = 0
             else:
-                if (stateString, action) in self.qValues:
-                    exploreOrExploit = self.qValues[(stateString, action)] + \
-                        self.policies[stateString][action] * \
+                if (stateString, selection) in self.qValues:
+                    score = self.qValues[(stateString, selection)] + \
+                        self.policies[stateString][selection] * \
                         sqrt(self.nodeVisitQuantity.get(stateString, 0)) / \
-                        (self.edgeVisitQuantity.get((stateString, action), 0) + 1)
+                        (self.edgeVisitQuantity.get((stateString, selection), 0) + 1)
                 else:
-                    exploreOrExploit = self.policies[stateString][action] * \
+                    score = self.policies[stateString][selection] * \
                         sqrt(self.nodeVisitQuantity.get(stateString, 0))
 
-                if exploreOrExploit > bestScore:
-                    bestScore = exploreOrExploit
-                    bestAction = action
-                    bestActionStr = actionString
-                    bestActionArr = actionStateTuple
+                if score > bestScore:
+                    bestScore = score
+                    bestSelection = selection
+                    bestSelectionStr = selectionString
+                    bestSelectionArr = selectionTuple
 
-        if bestActionStr not in self.policies:
-            movementTensor = torch.tensor(bestActionArr, dtype=torch.float)
-            self.policies[stateString], value = self.nets[0](bestActionArr)
-            validSelections = torch.tensor(self.env.getPositionOfAmazons())
+        if bestSelectionStr not in self.policies:
+            movementTensor = torch.tensor(
+                bestSelectionArr, dtype=torch.float).unsqueeze(0)
+            self.policies[bestSelectionStr], values[1] = self.nets[1](
+                movementTensor)
 
-            self.policies[stateString] *= validSelections  # mask out invalids
+            # mask out invalids
+            self.policies[bestSelectionStr] *= torch.tensor(
+                self.valids[bestSelectionStr])
+
+        validCoordinates = np.transpose(
+            np.nonzero(self.valids[bestSelectionStr]))
+        bestScore = float("-inf")
+        bestMove = None
+
+        # Choose coord to move to
+        for moveTo in validCoordinates:
+            moveTo = tuple(coord.item() for coord in moveTo)
+            if (bestSelectionStr, moveTo) in self.qValues:
+                score = self.qValues[(bestSelectionStr, moveTo)] + \
+                    self.policies[bestSelectionStr][moveTo] * \
+                    sqrt(self.nodeVisitQuantity.get(bestSelectionStr, 0)) / \
+                    (self.edgeVisitQuantity.get((bestSelectionStr, moveTo), 0) + 1)
+            else:
+                score = self.policies[bestSelectionStr][moveTo] * \
+                    sqrt(self.nodeVisitQuantity.get(bestSelectionStr, 0))
+
+            if score > bestScore:
+                bestScore = score
+                bestMove = moveTo
+
+        bestMoveString = bestSelectionStr + "".join(str(bestMove))
+        shotTensor = torch.tensor(bestSelectionArr, dtype=torch.float)
+        shotTensor[0][bestSelection], shotTensor[0][bestMove] = 0, 1
+        shotTensor[3][bestSelection], shotTensor[3][bestMove] = 0, 1
+        shotTensor.unsqueeze_(0)
+
+        # New leaf node
+        if bestMoveString not in self.policies:
+            self.policies[bestMoveString], values[2] = self.nets[2](shotTensor)
+            validSelections = torch.tensor(
+                self.env.getValidShotsFromNewPos(bestMove, bestSelection))
+
+            # mask out invalids
+            self.policies[bestMoveString] *= validSelections
 
             validCoordinates = np.nonzero(validSelections)
-            self.valids[stateString] = validCoordinates
-            return -value
+            self.valids[bestMoveString] = validCoordinates
+            return -values
+
+        validShots = self.valids[bestMoveString]
+        bestScore = float("-inf")
+        bestShot = None
+
+        for shot in validShots:
+            shot = tuple(coord.item() for coord in shot)
+            if (bestMoveString, shot) in self.qValues:
+                score = self.qValues[(bestMoveString, shot)] + \
+                    self.policies[bestMoveString][shot] * \
+                    sqrt(self.nodeVisitQuantity.get(bestMoveString, 0)) / \
+                    (self.edgeVisitQuantity.get((bestMoveString, shot), 0) + 1)
+            else:
+                score = self.policies[bestMoveString][shot] * \
+                    sqrt(self.nodeVisitQuantity.get(bestMoveString, 0))
+
+            if score > bestScore:
+                bestScore = score
+                bestShot = shot
+
+        print("Descending through: " +
+              f"({bestSelection}), ({bestMove}), ({bestShot})")
+
+        self.env.move(bestSelection, bestMove, bestShot)
+
+        values = self.search()
+        pairs = ((stateString, bestSelection),
+                 (bestSelectionStr, bestMove),
+                 (bestMoveString, bestShot))
+
+        for i in range(3):
+            if pairs[i] in self.qValues:
+                self.qValues[pairs[i]] = (
+                    self.self.qValues[pairs[i]] *
+                    self.edgeVisitQuantity[pairs[i]] +
+                    values[i]) / \
+                    self.edgeVisitQuantity[pairs[i]] + 1
+
+                self.edgeVisitQuantity[pairs[i]] += 1
+
+            else:
+                self.qValues[pairs[i]] = values[i]
+                self.edgeVisitQuantity[pairs[i]] = 1
+
+            self.nodeVisitQuantity[pairs[i][0]] += 1
+
+        return -values
 
 
 if __name__ == "__main__":
+    # Just for in-dev testing
     env = Environment()
     n1 = NeuralNet()
     n2 = NeuralNet(4)
@@ -92,3 +181,7 @@ if __name__ == "__main__":
     mcts = mcts(env, (n1, n2, n3))
     mcts.search()
     mcts.search()
+    mcts.search()
+    mcts.search()
+
+    env.kill()
