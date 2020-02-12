24f467590f0cb2edb9b1106a178d32744858d822
unknown <guymccombe@gmail.com>
2020-02-12 17:55:39 +0000

    Fixed bug causing all policy outputs to be 1

--- src/main/python/neuralNet.py
+++ src/main/python/neuralNet.py
@@ -41,7 +41,7 @@ class NeuralNet(nn.Module):
         layers.append(nn.BatchNorm2d(1))
         layers.append(nn.ReLU())
         layers.append(nn.Linear(10, 10))
-        layers.append(nn.Softmax2d())
+        layers.append(nn.Softmax(-1))
         return layers
 
     def __valueHead(self):
